96dd66ab7732e0e271888fa706d7bd7c785bc2ae
Nicki Kielsen <cph-nk11@cphbusiness.dk>
2022-04-06 12:20:36 +0200

    tilføj IBottomMapper og ButtomMapper

--- src/main/java/dat/startcode/model/persistence/BottomMapper.java
+++ src/main/java/dat/startcode/model/persistence/BottomMapper.java
@@ -1,53 +1,52 @@
 package dat.startcode.model.persistence;
 
+import dat.startcode.model.entities.Bottom;
 import dat.startcode.model.entities.Topping;
-import dat.startcode.model.entities.User;
 import dat.startcode.model.exceptions.DatabaseException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
 
-public class ToppingMapper implements IToppingMapper{
+public class BottomMapper implements IBottomMapper{
 
     ConnectionPool connectionPool;
 
-    public ToppingMapper(ConnectionPool connectionPool)
+    public BottomMapper(ConnectionPool connectionPool)
     {
         this.connectionPool = connectionPool;
     }
 
     @Override
-    public Topping getToppingbyId(int topping_id) throws DatabaseException {
+    public Bottom getBottomById(int bottom_id) throws DatabaseException {
 //        Logger.getLogger("web").log(Level.INFO, "");
 
-        Topping topping = null;
+        Bottom bottom = null;
 
-        String sql = "SELECT * FROM cupcake.topping WHERE topping_id = ?;";
+        String sql = "SELECT * FROM cupcake.bottom WHERE bottom_id = ?;";
 
         try (Connection connection = connectionPool.getConnection())
         {
             try (PreparedStatement ps = connection.prepareStatement(sql))
             {
-                ps.setInt(1, topping_id);
+                ps.setInt(1, bottom_id);
                 ResultSet rs = ps.executeQuery();
                 if (rs.next())
                 {
-                    int toppingId = rs.getInt("topping_id");
+                    int toppingId = rs.getInt("bottom_id");
                     String flavor = rs.getString("flavor");
                     int price = rs.getInt("price");
-                    topping = new Topping(toppingId, flavor, price);
+                    bottom = new Bottom(toppingId, flavor, price);
                 } else
                 {
-                    throw new DatabaseException("Error. No such Topping_id");
+                    throw new DatabaseException("Error. No such Bottom_id");
                 }
             }
         } catch (SQLException ex)
         {
             throw new DatabaseException(ex, "Error logging in. Something went wrong with the database");
         }
-        return topping;
+        return bottom;
     }
 }
